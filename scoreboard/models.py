06e5d2e9ca42d6787a23e9fe11cbe30641884d64
Jorick Spitzen <jorick1810@gmail.com>
2017-01-28 22:22:58 +0100

    Added color to teams

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from colorful.fields import RGBColorField
+
 
 class Team(models.Model):
     name = models.CharField(max_length=128)
+    color = RGBColorField(null=True, blank=True)
 
     def __str__(self):
         return self.name
